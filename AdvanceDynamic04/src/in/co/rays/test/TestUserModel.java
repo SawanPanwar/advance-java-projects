b4d7449b3c98cefc7231bb0465a270d7341acdb1
SawanPanwar <skpanwar25@gmail.com>
2023-12-22 17:29:29 +0530

    done

--- AdvanceDynamic04/src/in/co/rays/test/TestUserModel.java
+++ AdvanceDynamic04/src/in/co/rays/test/TestUserModel.java
@@ -1,12 +1,5 @@
 package in.co.rays.test;
 
-import java.text.SimpleDateFormat;
-import java.util.Iterator;
-import java.util.List;
-
-import in.co.rays.bean.UserBean;
-import in.co.rays.model.UserModel;
-
 public class TestUserModel {
 
 	public static void main(String[] args) throws Exception {
