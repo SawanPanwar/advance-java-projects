02ad6069c98b02891b1cbe60e7eeccadc6345565
SawanPanwar <skpanwar25@gmail.com>
2024-01-08 11:54:49 +0530

    done

---
+++ advance-java-practicals/src/in/co/rays/test/TestRead.java
@@ -0,0 +1,30 @@
+package in.co.rays.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class TestRead {
+
+	public static void main(String[] args) throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/advance11", "root", "root");
+
+		Statement stmt = conn.createStatement();
+
+		ResultSet rs = stmt.executeQuery("select * from marksheet where id = 10");
+
+		while (rs.next()) {
+			System.out.print(rs.getInt(1));
+			System.out.print("\t" + rs.getInt(2));
+			System.out.print("\t" + rs.getString(3));
+			System.out.print("\t" + rs.getInt(4));
+			System.out.print("\t" + rs.getInt(5));
+			System.out.println("\t" + rs.getInt(6));
+		}
+	}
+
+}
