02bf57af7bd2186a6cf2082630e10e0d9b1129d2
SawanPanwar <skpanwar25@gmail.com>
2024-01-10 12:21:25 +0530

    done

---
+++ advance-java-practicals/src/in/co/rays/marksheet/MarksheetModel.java
@@ -0,0 +1,143 @@
+package in.co.rays.marksheet;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarksheetModel {
+
+	public void add(MarksheetBean bean) throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/advance11", "root", "root");
+
+		PreparedStatement pstmt = conn.prepareStatement("insert into marksheet values(?, ?, ?, ?, ?, ?)");
+
+		pstmt.setInt(1, bean.getId());
+		pstmt.setInt(2, bean.getRollNo());
+		pstmt.setString(3, bean.getName());
+		pstmt.setInt(4, bean.getPhysics());
+		pstmt.setInt(5, bean.getChemistry());
+		pstmt.setInt(6, bean.getMaths());
+
+		int i = pstmt.executeUpdate();
+
+		System.out.println("Data Inserted = " + i);
+
+	}
+
+	public void update(MarksheetBean bean) throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/advance11", "root", "root");
+
+		PreparedStatement pstmt = conn.prepareStatement(
+				"update marksheet set roll_no = ?, name = ?, physics = ?, chemistry = ?, maths = ? where id = ?");
+
+		pstmt.setInt(1, bean.getRollNo());
+		pstmt.setString(2, bean.getName());
+		pstmt.setInt(3, bean.getPhysics());
+		pstmt.setInt(4, bean.getChemistry());
+		pstmt.setInt(5, bean.getMaths());
+		pstmt.setInt(6, bean.getId());
+
+		int i = pstmt.executeUpdate();
+
+		System.out.println("Data Updated = " + i);
+
+	}
+
+	public void delete(int id) throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/advance11", "root", "root");
+
+		PreparedStatement pstmt = conn.prepareStatement("delete from marksheet where id = ?");
+
+		pstmt.setInt(1, id);
+
+		int i = pstmt.executeUpdate();
+
+		System.out.println("Data Deleted = " + i);
+
+	}
+
+	public void readAll() throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/advance11", "root", "root");
+
+		PreparedStatement pstmt = conn.prepareStatement("select * from marksheet");
+
+		ResultSet rs = pstmt.executeQuery();
+
+		while (rs.next()) {
+			System.out.print(rs.getInt(1));
+			System.out.print("\t" + rs.getInt(2));
+			System.out.print("\t" + rs.getString(3));
+			System.out.print("\t" + rs.getInt(4));
+			System.out.print("\t" + rs.getInt(5));
+			System.out.println("\t" + rs.getInt(6));
+		}
+
+	}
+
+	public MarksheetBean findByPk(int id) throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/advance11", "root", "root");
+
+		PreparedStatement pstmt = conn.prepareStatement("select * from marksheet where id = ?");
+
+		pstmt.setInt(1, id);
+
+		ResultSet rs = pstmt.executeQuery();
+
+		MarksheetBean bean = null;
+
+		while (rs.next()) {
+			bean = new MarksheetBean();
+			bean.setId(rs.getInt(1));
+			bean.setRollNo(rs.getInt(2));
+			bean.setName(rs.getString(3));
+			bean.setPhysics(rs.getInt(4));
+			bean.setChemistry(rs.getInt(5));
+			bean.setMaths(rs.getInt(6));
+		}
+		return bean;
+	}
+
+	public List search() throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/advance11", "root", "root");
+
+		PreparedStatement pstmt = conn.prepareStatement("select * from marksheet");
+
+		ResultSet rs = pstmt.executeQuery();
+
+		List list = new ArrayList();
+
+		while (rs.next()) {
+			MarksheetBean bean = new MarksheetBean();
+			bean.setId(rs.getInt(1));
+			bean.setRollNo(rs.getInt(2));
+			bean.setName(rs.getString(3));
+			bean.setPhysics(rs.getInt(4));
+			bean.setChemistry(rs.getInt(5));
+			bean.setMaths(rs.getInt(6));
+			list.add(bean);
+		}
+		return list;
+	}
+
+}
--- advance-java-practicals/src/in/co/rays/marksheet/TestMarksheet.java
+++ advance-java-practicals/src/in/co/rays/marksheet/TestMarksheet.java
@@ -1,13 +1,79 @@
 package in.co.rays.marksheet;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.util.Iterator;
+import java.util.List;
 
 public class TestMarksheet {
 
 	public static void main(String[] args) throws Exception {
-		
+
+		// testAdd();
+		// testUpdate();
+		// testDelete();
+		// testReadAll();
+		// testFindByPk();
+		testSearch();
+
+	}
+
+	private static void testSearch() throws Exception {
+
+		MarksheetModel model = new MarksheetModel();
+
+		List list = model.search();
+
+		Iterator it = list.iterator();
+
+		while (it.hasNext()) {
+
+			MarksheetBean bean = (MarksheetBean) it.next();
+
+			System.out.println(bean.getId());
+			System.out.println(bean.getRollNo());
+			System.out.println(bean.getName());
+			System.out.println(bean.getPhysics());
+			System.out.println(bean.getChemistry());
+			System.out.println(bean.getMaths());
+
+		}
+
+	}
+
+	private static void testFindByPk() throws Exception {
+
+		MarksheetModel model = new MarksheetModel();
+
+		MarksheetBean bean = model.findByPk(20);
+
+		if (bean != null) {
+			System.out.println(bean.getId());
+			System.out.println(bean.getRollNo());
+			System.out.println(bean.getName());
+			System.out.println(bean.getPhysics());
+			System.out.println(bean.getChemistry());
+			System.out.println(bean.getMaths());
+		} else {
+			System.out.println("ID not exist..!!!");
+		}
+	}
+
+	private static void testReadAll() throws Exception {
+
+		MarksheetModel model = new MarksheetModel();
+
+		model.readAll();
+
+	}
+
+	private static void testDelete() throws Exception {
+
+		MarksheetModel model = new MarksheetModel();
+
+		model.delete(10);
+
+	}
+
+	private static void testUpdate() throws Exception {
 		MarksheetBean bean = new MarksheetBean();
 		bean.setId(15);
 		bean.setRollNo(115);
@@ -16,28 +82,25 @@ public class TestMarksheet {
 		bean.setChemistry(78);
 		bean.setMaths(67);
 
-		testAdd(bean);
+		MarksheetModel model = new MarksheetModel();
+
+		model.update(bean);
 
 	}
 
-	private static void testAdd(MarksheetBean bean) throws Exception {
+	private static void testAdd() throws Exception {
 
-		Class.forName("com.mysql.cj.jdbc.Driver");
+		MarksheetBean bean = new MarksheetBean();
+		bean.setId(15);
+		bean.setRollNo(115);
+		bean.setName("qwe");
+		bean.setPhysics(87);
+		bean.setChemistry(78);
+		bean.setMaths(67);
 
-		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/advance11", "root", "root");
+		MarksheetModel model = new MarksheetModel();
 
-		PreparedStatement pstmt = conn.prepareStatement("insert into marksheet values(?, ?, ?, ?, ?, ?)");
-
-		pstmt.setInt(1, bean.getId());
-		pstmt.setInt(2, bean.getRollNo());
-		pstmt.setString(3, bean.getName());
-		pstmt.setInt(4, bean.getPhysics());
-		pstmt.setInt(5, bean.getChemistry());
-		pstmt.setInt(6, bean.getMaths());
-
-		int i = pstmt.executeUpdate();
-
-		System.out.println("Data Inserted = " + i);
+		model.add(bean);
 
 	}
 
