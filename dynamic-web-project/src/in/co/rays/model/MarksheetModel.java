1152e3ce6bdfc3766d072d3be33735ddbc89203c
SawanPanwar <skpanwar25@gmail.com>
2024-03-06 17:08:48 +0530

    done

---
+++ dynamic-web-project/src/in/co/rays/model/MarksheetModel.java
@@ -0,0 +1,191 @@
+package in.co.rays.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import in.co.rays.bean.MarksheetBean;
+import in.co.rays.util.JDBCDataSource;
+
+public class MarksheetModel {
+
+	public Integer nextPk() throws Exception {
+
+		int pk = 0;
+
+		Connection conn = JDBCDataSource.getConnection();
+
+		PreparedStatement ps = conn.prepareStatement("select max(id) from marksheet");
+
+		ResultSet rs = ps.executeQuery();
+
+		while (rs.next()) {
+
+			pk = rs.getInt(1);
+
+		}
+		conn.close();
+		return pk + 1;
+	}
+
+	public void add(MarksheetBean bean) throws Exception {
+
+		int pk = nextPk();
+
+		Connection conn = JDBCDataSource.getConnection();
+
+		PreparedStatement ps = conn.prepareStatement("insert into marksheet values(?, ?, ?, ?, ?, ?)");
+
+		ps.setInt(1, pk);
+		ps.setInt(2, bean.getRollNo());
+		ps.setString(3, bean.getName());
+		ps.setInt(4, bean.getPhysics());
+		ps.setInt(5, bean.getChemistry());
+		ps.setInt(6, bean.getMaths());
+
+		int i = ps.executeUpdate();
+		
+		conn.close();
+
+		System.out.println("data inserted = " + i);
+
+	}
+
+	public void update(MarksheetBean bean) throws Exception {
+
+		Connection conn = JDBCDataSource.getConnection();
+
+		PreparedStatement ps = conn.prepareStatement(
+				"update marksheet set roll_no = ?, name = ?, physics = ?, chemistry = ?, maths = ? where id = ?");
+
+		ps.setInt(1, bean.getRollNo());
+		ps.setString(2, bean.getName());
+		ps.setInt(3, bean.getPhysics());
+		ps.setInt(4, bean.getChemistry());
+		ps.setInt(5, bean.getMaths());
+		ps.setInt(6, bean.getId());
+
+		int i = ps.executeUpdate();
+		
+		conn.close();
+
+		System.out.println("data updated = " + i);
+
+	}
+
+	public void delete(int id) throws Exception {
+
+		Connection conn = JDBCDataSource.getConnection();
+
+		PreparedStatement ps = conn.prepareStatement("delete from marksheet where id = ?");
+
+		ps.setInt(1, id);
+
+		int i = ps.executeUpdate();
+		
+		conn.close();
+
+		System.out.println("data deleted = " + i);
+
+	}
+
+	public List searchSimple() throws Exception {
+
+		Connection conn = JDBCDataSource.getConnection();
+
+		PreparedStatement ps = conn.prepareStatement("select * from marksheet");
+
+		ResultSet rs = ps.executeQuery();
+
+		List list = new ArrayList();
+
+		while (rs.next()) {
+			MarksheetBean bean = new MarksheetBean();
+			bean.setId(rs.getInt(1));
+			bean.setRollNo(rs.getInt(2));
+			bean.setName(rs.getString(3));
+			bean.setPhysics(rs.getInt(4));
+			bean.setChemistry(rs.getInt(5));
+			bean.setMaths(rs.getInt(6));
+			list.add(bean);
+		}
+		
+		conn.close();
+
+		return list;
+
+	}
+
+	public List search(MarksheetBean bean, int pageNo, int pageSize) throws Exception {
+
+		Connection conn = JDBCDataSource.getConnection();
+
+		StringBuffer sql = new StringBuffer("select * from marksheet where 1=1");
+
+		if (bean != null) {
+
+			if (bean.getId() > 0) {
+				sql.append(" and id = " + bean.getId());
+			}
+
+			if (bean.getName() != null && bean.getName().length() > 0) {
+				sql.append(" and name like '" + bean.getName() + "%'");
+			}
+
+		}
+		if (pageSize > 0) {
+			pageNo = (pageNo - 1) * pageSize;
+			sql.append(" limit " + pageNo + ", " + pageSize);
+		}
+
+		System.out.println("sql => " + sql);
+
+		PreparedStatement ps = conn.prepareStatement(sql.toString());
+
+		ResultSet rs = ps.executeQuery();
+
+		List list = new ArrayList();
+
+		while (rs.next()) {
+			bean = new MarksheetBean();
+			bean.setId(rs.getInt(1));
+			bean.setRollNo(rs.getInt(2));
+			bean.setName(rs.getString(3));
+			bean.setPhysics(rs.getInt(4));
+			bean.setChemistry(rs.getInt(5));
+			bean.setMaths(rs.getInt(6));
+			list.add(bean);
+		}
+		
+		conn.close();
+
+		return list;
+
+	}
+
+	public MarksheetBean findByPk(int id) throws Exception {
+
+		Connection conn = JDBCDataSource.getConnection();
+
+		PreparedStatement ps = conn.prepareStatement("select * from marksheet where id = ?");
+
+		ps.setInt(1, id);
+
+		ResultSet rs = ps.executeQuery();
+
+		MarksheetBean bean = new MarksheetBean();
+
+		while (rs.next()) {
+			bean.setId(rs.getInt(1));
+			bean.setRollNo(rs.getInt(2));
+			bean.setName(rs.getString(3));
+			bean.setPhysics(rs.getInt(4));
+			bean.setChemistry(rs.getInt(5));
+			bean.setMaths(rs.getInt(6));
+		}
+		conn.close();
+		return bean;
+	}
+}
