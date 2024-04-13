15952a03bac4bc0dfcd956920e2d9c1f471a4622
SawanPanwar <skpanwar25@gmail.com>
2024-04-13 14:02:06 +0530

    done

--- advance-java-10/src/in/co/rays/user/TestUser.java
+++ advance-java-10/src/in/co/rays/user/TestUser.java
@@ -2,12 +2,16 @@ package in.co.rays.user;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 public class TestUser {
 
 	public static void main(String[] args) throws Exception {
 
-		testAdd();
+		// testAdd();
+
+		testSearch();
 
 	}
 
@@ -27,6 +31,36 @@ public class TestUser {
 		UserModel model = new UserModel();
 
 		model.add(bean);
+
+	}
+
+	public static void testSearch() throws Exception {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+
+		UserBean bean = new UserBean();
+		// bean.setFirstName("pqr");
+		bean.setDob(sdf.parse("13-04-2024"));
+
+		UserModel model = new UserModel();
+
+		List list = model.search(bean, 1, 5);
+
+		Iterator it = list.iterator();
+
+		while (it.hasNext()) {
+
+			bean = (UserBean) it.next();
+
+			System.out.print(bean.getId());
+			System.out.print("\t" + bean.getFirstName());
+			System.out.print("\t" + bean.getLastName());
+			System.out.print("\t" + bean.getLoginId());
+			System.out.print("\t" + bean.getPassword());
+			System.out.print("\t" + bean.getDob());
+			System.out.println("\t" + bean.getAddress());
+
+		}
 
 	}
 
--- advance-java-10/src/in/co/rays/user/UserModel.java
+++ advance-java-10/src/in/co/rays/user/UserModel.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserModel {
 
@@ -50,6 +52,53 @@ public class UserModel {
 		int i = pstmt.executeUpdate();
 
 		System.out.println("Data Inserted = " + i);
+	}
+
+	public List search(UserBean bean, int pageNo, int pageSize) throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/advance_java", "root", "root");
+
+		StringBuffer sql = new StringBuffer("select * from user where 1=1");
+
+		if (bean != null) {
+
+			if (bean.getFirstName() != null && bean.getFirstName().length() > 0) {
+				sql.append(" and first_name like '" + bean.getFirstName() + "%'");
+			}
+
+			if (bean.getDob() != null && bean.getDob().getTime() > 0) {
+				sql.append(" and dob like '" + new java.sql.Date(bean.getDob().getTime()) + "%'");
+			}
+
+		}
+
+		if (pageSize > 0) {
+			pageNo = (pageNo - 1) * pageSize;
+			sql.append(" limit " + pageNo + ", " + pageSize);
+		}
+
+		System.out.println("sql => " + sql);
+
+		PreparedStatement pstmt = conn.prepareStatement(sql.toString());
+
+		ResultSet rs = pstmt.executeQuery();
+
+		List list = new ArrayList();
+
+		while (rs.next()) {
+			bean = new UserBean();
+			bean.setId(rs.getInt(1));
+			bean.setFirstName(rs.getString(2));
+			bean.setLastName(rs.getString(3));
+			bean.setLoginId(rs.getString(4));
+			bean.setPassword(rs.getString(5));
+			bean.setDob(rs.getDate(6));
+			bean.setAddress(rs.getString(7));
+			list.add(bean);
+		}
+		return list;
 	}
 
 }
