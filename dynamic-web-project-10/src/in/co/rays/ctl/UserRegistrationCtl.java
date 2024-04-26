1ae605c112d7430de45d4788b2f654b836260cad
SawanPanwar <skpanwar25@gmail.com>
2024-04-26 12:25:48 +0530

    done

---
+++ dynamic-web-project-10/src/in/co/rays/ctl/UserRegistrationCtl.java
@@ -0,0 +1,67 @@
+package in.co.rays.ctl;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import in.co.rays.bean.UserBean;
+import in.co.rays.model.UserModel;
+
+@WebServlet("/UserRegistrationCtl")
+public class UserRegistrationCtl extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+		String firstName = req.getParameter("firstName");
+		String lastName = req.getParameter("lastName");
+		String loginId = req.getParameter("loginId");
+		String password = req.getParameter("password");
+		String dob = req.getParameter("dob");
+		String address = req.getParameter("address");
+
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(loginId);
+		System.out.println(password);
+		System.out.println(dob);
+		System.out.println(address);
+
+		UserBean bean = new UserBean();
+		bean.setFirstName(firstName);
+		bean.setLastName(lastName);
+		bean.setLoginId(loginId);
+		bean.setPassword(password);
+		try {
+			bean.setDob(sdf.parse(dob));
+		} catch (ParseException e) {
+			System.out.println(e);
+		}
+		bean.setAddress(address);
+
+		UserModel model = new UserModel();
+
+		try {
+			model.add(bean);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		resp.sendRedirect("UserRegistrationView.jsp");
+
+	}
+
+}
