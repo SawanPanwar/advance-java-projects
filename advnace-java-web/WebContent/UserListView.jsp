dc338a43200d5755dae7fc2b4d096c6f3523c643
SawanPanwar <skpanwar25@gmail.com>
2024-08-07 16:06:20 +0530

    done

--- advnace-java-web/WebContent/UserListView.jsp
+++ advnace-java-web/WebContent/UserListView.jsp
@@ -12,12 +12,22 @@
 <body>
 	<%
 		List list = (List) request.getAttribute("list");
+		List nextList = (List) request.getAttribute("nextList");
+		int pageNo = (int) request.getAttribute("pageNo");
 	%>
 	<%@ include file="Header.jsp"%>
 	<form action="UserListCtl" method="post">
 		<div align="center">
 			<h1>User List</h1>
 		</div>
+		<table>
+			<tr>
+				<td><input type="text" name="firstName"></td>
+				<td><input type="date" name="dob"></td>
+				<td><input type="submit" name="operation" value="search"></td>
+			</tr>
+		</table>
+		<br>
 		<table border="1%" style="width: 100%">
 			<tr>
 				<th>Id</th>
@@ -46,6 +56,21 @@
 				}
 			%>
 		</table>
+		<br>
+		<table style="width: 100%">
+			<tr>
+				<td style="width: 30%"><input type="submit" name="operation"
+					value="previous" <%=(pageNo == 1) ? "disabled" : ""%>></td>
+				<td style="width: 30%"><input type="submit" name="operation"
+					value="add"></td>
+				<td style="width: 25%"><input type="submit" name="operation"
+					value="delete"></td>
+				<td style="text-align: right;"><input type="submit"
+					name="operation" value="next"
+					<%=(nextList.size() == 0) ? "disabled" : ""%>></td>
+			</tr>
+		</table>
+		<input type="hidden" name="pageNo" value="<%=pageNo%>">
 	</form>
 </body>
 </html>
--- advnace-java-web/src/in/co/rays/ctl/UserListCtl.java
+++ advnace-java-web/src/in/co/rays/ctl/UserListCtl.java
@@ -1,6 +1,8 @@
 package in.co.rays.ctl;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import in.co.rays.bean.UserBean;
 import in.co.rays.model.UserModel;
 
 @WebServlet("/UserListCtl")
@@ -26,12 +29,15 @@ public class UserListCtl extends HttpServlet {
 
 		try {
 			List list = model.search(null, pageNo, pageSize);
+			List nextList = model.search(null, pageNo + 1, pageSize);
 
 			req.setAttribute("list", list);
+			req.setAttribute("nextList", nextList);
+			req.setAttribute("pageNo", pageNo);
 
 			RequestDispatcher rd = req.getRequestDispatcher("UserListView.jsp");
 			rd.forward(req, resp);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -40,6 +46,63 @@ public class UserListCtl extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+		String op = req.getParameter("operation");
+		String firstName = req.getParameter("firstName");
+		String dob = req.getParameter("dob");
+
+		int pageNo = 1;
+
+		pageNo = Integer.parseInt(req.getParameter("pageNo"));
+
+		int pageSize = 5;
+
+		UserBean bean = null;
+
+		UserModel model = new UserModel();
+
+		if (op.equals("search")) {
+			bean = new UserBean();
+			bean.setFirstName(firstName);
+			try {
+				bean.setDob(sdf.parse(dob));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
+
+		if (op.equals("previous")) {
+			pageNo--;
+		}
+
+		if (op.equals("next")) {
+			pageNo++;
+		}
+
+		if (op.equals("add")) {
+			resp.sendRedirect("UserCtl");
+		}
+
+		if (op.equals("delete")) {
+
+		}
+
+		try {
+			List list = model.search(bean, pageNo, pageSize);
+			List nextList = model.search(bean, pageNo + 1, pageSize);
+
+			req.setAttribute("list", list);
+			req.setAttribute("nextList", nextList);
+			req.setAttribute("pageNo", pageNo);
+
+			RequestDispatcher rd = req.getRequestDispatcher("UserListView.jsp");
+			rd.forward(req, resp);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 }
