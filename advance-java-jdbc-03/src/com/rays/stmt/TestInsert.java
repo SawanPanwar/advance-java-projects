d681bfbebaba4eb0694d3407077ed5f8df46b987
SawanPanwar <skpanwar25@gmail.com>
2024-11-09 16:12:39 +0530

    done

---
+++ advance-java-jdbc-03/src/com/rays/stmt/TestInsert.java
@@ -0,0 +1,21 @@
+package com.rays.stmt;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class TestInsert {
+
+	public static void main(String[] args) throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/advance_java", "root", "root");
+
+		Statement stmt = conn.createStatement();
+
+		int i = stmt.executeUpdate("insert into marksheet values(24, 124, 'chanchal', 89, 88, 78)");
+
+		System.out.println("data inserted => " + i);
+	}
+}
---
+++ advance-java-jdbc-03/src/com/rays/stmt/TestUpdate.java
@@ -0,0 +1,21 @@
+package com.rays.stmt;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class TestUpdate {
+
+	public static void main(String[] args) throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/advance_java", "root", "root");
+
+		Statement stmt = conn.createStatement();
+
+		int i = stmt.executeUpdate("update marksheet set name = 'chandu' where id = 24");
+
+		System.out.println("data updated => " + i);
+	}
+}
