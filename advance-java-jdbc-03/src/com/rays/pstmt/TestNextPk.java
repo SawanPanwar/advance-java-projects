492cb98ed755a7b518a50b5617897f1ab0f72d6f
SawanPanwar <skpanwar25@gmail.com>
2024-11-12 15:58:08 +0530

    done

---
+++ advance-java-jdbc-03/src/com/rays/pstmt/TestNextPk.java
@@ -0,0 +1,29 @@
+package com.rays.pstmt;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class TestNextPk {
+
+	public static void main(String[] args) throws Exception {
+
+		int pk = 0;
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/advance_java", "root", "root");
+
+		PreparedStatement pstmt = conn.prepareStatement("select max(id) from marksheet");
+
+		ResultSet rs = pstmt.executeQuery();
+
+		while (rs.next()) {
+			pk = rs.getInt(1);
+		}
+
+		System.out.println(pk + 1);
+
+	}
+}
