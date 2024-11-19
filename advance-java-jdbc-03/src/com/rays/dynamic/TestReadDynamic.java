f8cce7877828c40e962e2d0aa46e08d04199b5c6
SawanPanwar <skpanwar25@gmail.com>
2024-11-19 13:42:14 +0530

    done

--- advance-java-jdbc-03/src/com/rays/dynamic/TestReadDynamic.java
+++ advance-java-jdbc-03/src/com/rays/dynamic/TestReadDynamic.java
@@ -9,11 +9,21 @@ public class TestReadDynamic {
 
 	public static void main(String[] args) throws Exception {
 
-		testRead();
+		// testRead1();
 
+		MarksheetBean bean = new MarksheetBean();
+		// bean.setId(100);
+		// bean.setRollNo(101);
+		bean.setName("a");
+
+		// testRead2();
+
+		// testRead3(bean);
+
+		testRead4(bean, 2, 5);
 	}
 
-	public static void testRead() throws Exception {
+	public static void testRead1() throws Exception {
 
 		Class.forName("com.mysql.cj.jdbc.Driver");
 
@@ -31,9 +41,122 @@ public class TestReadDynamic {
 			System.out.print("\t" + rs.getInt(4));
 			System.out.print("\t" + rs.getInt(5));
 			System.out.println("\t" + rs.getInt(6));
-
 		}
-
 	}
 
+	public static void testRead2(MarksheetBean bean) throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/advance_java", "root", "root");
+
+		StringBuffer sql = new StringBuffer("select * from marksheet");
+
+		if (bean != null) {
+			if (bean.getId() > 0) {
+				sql.append(" where id = " + bean.getId());
+			}
+			if (bean.getRollNo() > 0) {
+				sql.append(" where roll_no = " + bean.getRollNo());
+			}
+			if (bean.getName() != null && bean.getName().length() > 0) {
+				sql.append(" where name like '" + bean.getName() + "%'");
+			}
+		}
+
+		System.out.println("sql => " + sql);
+
+		PreparedStatement pstmt = conn.prepareStatement(sql.toString());
+
+		ResultSet rs = pstmt.executeQuery();
+
+		while (rs.next()) {
+
+			System.out.print(rs.getInt(1));
+			System.out.print("\t" + rs.getInt(2));
+			System.out.print("\t" + rs.getString(3));
+			System.out.print("\t" + rs.getInt(4));
+			System.out.print("\t" + rs.getInt(5));
+			System.out.println("\t" + rs.getInt(6));
+		}
+	}
+
+	public static void testRead3(MarksheetBean bean) throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/advance_java", "root", "root");
+
+		StringBuffer sql = new StringBuffer("select * from marksheet where 1=1");
+
+		if (bean != null) {
+			if (bean.getId() > 0) {
+				sql.append(" and id = " + bean.getId());
+			}
+			if (bean.getRollNo() > 0) {
+				sql.append(" and roll_no = " + bean.getRollNo());
+			}
+			if (bean.getName() != null && bean.getName().length() > 0) {
+				sql.append(" and name like '" + bean.getName() + "%'");
+			}
+		}
+
+		System.out.println("sql => " + sql);
+
+		PreparedStatement pstmt = conn.prepareStatement(sql.toString());
+
+		ResultSet rs = pstmt.executeQuery();
+
+		while (rs.next()) {
+
+			System.out.print(rs.getInt(1));
+			System.out.print("\t" + rs.getInt(2));
+			System.out.print("\t" + rs.getString(3));
+			System.out.print("\t" + rs.getInt(4));
+			System.out.print("\t" + rs.getInt(5));
+			System.out.println("\t" + rs.getInt(6));
+		}
+	}
+
+	public static void testRead4(MarksheetBean bean, int pageNo, int pageSize) throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/advance_java", "root", "root");
+
+		StringBuffer sql = new StringBuffer("select * from marksheet where 1=1");
+
+		if (bean != null) {
+			if (bean.getId() > 0) {
+				sql.append(" and id = " + bean.getId());
+			}
+			if (bean.getRollNo() > 0) {
+				sql.append(" and roll_no = " + bean.getRollNo());
+			}
+			if (bean.getName() != null && bean.getName().length() > 0) {
+				sql.append(" and name like '" + bean.getName() + "%'");
+			}
+		}
+
+		if (pageSize > 0) {
+			pageNo = ((pageNo - 1) * pageSize);
+			sql.append(" limit " + pageNo + ", " + pageSize);
+		}
+
+		System.out.println("sql => " + sql);
+
+		PreparedStatement pstmt = conn.prepareStatement(sql.toString());
+
+		ResultSet rs = pstmt.executeQuery();
+
+		while (rs.next()) {
+
+			System.out.print(rs.getInt(1));
+			System.out.print("\t" + rs.getInt(2));
+			System.out.print("\t" + rs.getString(3));
+			System.out.print("\t" + rs.getInt(4));
+			System.out.print("\t" + rs.getInt(5));
+			System.out.println("\t" + rs.getInt(6));
+		}
+	}
 }
