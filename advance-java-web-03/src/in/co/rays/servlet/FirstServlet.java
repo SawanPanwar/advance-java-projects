39d3ebf43a85c577f2ca06d23dfc1b4fd0dd60f7
SawanPanwar <skpanwar25@gmail.com>
2024-11-30 16:11:13 +0530

    30 november

--- advance-java-web-03/src/in/co/rays/servlet/FirstServlet.java
+++ advance-java-web-03/src/in/co/rays/servlet/FirstServlet.java
@@ -1,7 +1,6 @@
 package in.co.rays.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,31 +8,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/FirstServlet")
+@WebServlet(urlPatterns = { "/FirstServlet", "/First", "*.abc" })
 public class FirstServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+		System.out.println("in first servlet do get method");
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-		System.out.println(req.getParameter("firstName"));
-		System.out.println(req.getParameter("lastName"));
-		System.out.println(req.getParameter("loginId"));
-		System.out.println(req.getParameter("password"));
-		System.out.println(req.getParameter("dob"));
-		System.out.println(req.getParameter("address"));
-
-		resp.setContentType("text/html");
-
-		PrintWriter out = resp.getWriter();
-
-		out.print("<h1>" + req.getParameter("firstName") + "</h1>");
-
-		out.close();
-
+		System.out.println("in first servlet do post method");
 	}
+
 }
