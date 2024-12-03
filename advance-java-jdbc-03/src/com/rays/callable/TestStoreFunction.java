d505ff5543bbee3e56c3bfb86c3bf346a1eb285d
SawanPanwar <skpanwar25@gmail.com>
2024-12-03 15:15:20 +0530

    02 December

---
+++ advance-java-jdbc-03/src/com/rays/callable/TestStoreFunction.java
@@ -0,0 +1,24 @@
+package com.rays.callable;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Types;
+
+public class TestStoreFunction {
+
+	public static void main(String[] args) throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+
+		CallableStatement callStmt = conn.prepareCall("{? = CALL square(?)}");
+		callStmt.setInt(2, 10);
+		callStmt.registerOutParameter(1, Types.INTEGER);
+		callStmt.execute();
+
+		System.out.println(" Square " + callStmt.getInt(1));
+	}
+
+}
